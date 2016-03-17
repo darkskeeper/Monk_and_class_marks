5ff53633d8a27b1adad5f0b22366023e13db633d
darkskeeper <dartskeeper@mail.ru>
2016-03-18 01:04:48 +0300

    Done?

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <utility>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int mark;
+    string name;
+    set < pair <int, string> > s;
+    while ( T-- )
+    {
+        cin >> name;
+        cin >> mark;
+        s.insert( pair < int, string > (100 - mark, name) );
+    }
+    set < pair < int, string > >::iterator it;
+    for ( it = s.begin() ; it != s.end() ; ++it )
+    {
+        cout << it->second << " " << 100 - it->first << endl;
+    }
+    system ("pause");
+    return 0;
+}
